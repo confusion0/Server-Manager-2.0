3b25aacef5b19b19aca2e87a93ffd8f0abf0c659
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-05 15:04:50 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,16 @@
+const invite = `
+Invite bot: [Click Here](https://discord.com/api/oauth2/authorize?client_id=739943852726681650&permissions=8&scope=bot)
+Support Server: [Click Here](discord.gg/)
+`
+
+
 module.exports = {
   name: 'invite',
   aliases: [],
   run: async(Discord, client, message, args) => {
-    
+    const embed = new Discord.MessageEmbed()
+      .setTitle("Invite")
+      .setDescription()
+      message.channel.send(embed)
   }
 }
