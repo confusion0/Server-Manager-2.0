7d0d014eecc4fd69556220a0a379ef147460fac1
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-06 12:56:32 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -25,7 +25,7 @@ module.exports = {
         }
       }
     } catch (error) {
-       if (!(error instanceof DiscordAPIError: Unknown Message)) {
+       if (!(error.name === 'UnhandledPromiseRejectionWarning: DiscordAPIError: Unknown Message')) {
          throw error
        }
     }
