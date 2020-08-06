d6a99999891d8b48b9ce3f75b0d95c5f5aa9a630
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-06 11:33:24 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,22 +4,15 @@ module.exports = {
   name: 'purge',
   aliases: ['clear'],
   run: async(Discord, client, message, args) => {
-    let amount = args[0]
-    
     message.delete()
-
-    if (!amount) return (await message.reply('You haven\'t given an amount of messages which should be deleted!')).delete({timeout: timeout}); // Checks if the `amount` parameter is given
-    if (isNaN(amount)) return (await message.reply('The amount parameter isn`t a number!')).delete({timeout: timeout}); // Checks if the `amount` parameter is a number. If not, the command throws an error
-
-    if (amount > 100) return (await message.reply('You can`t delete more than 100 messages at once!')).delete({timeout: timeout}); // Checks if the `amount` integer is bigger than 100
-    if (amount < 1) return (await message.reply('You have to delete at least 1 message!')).delete({timeout: timeout}); // Checks if the `amount` integer is smaller than 1
-
-    await message.channel.messages.fetch({ limit: amount }).then(async messages => { // Fetches the messages
-      message.channel.bulkDelete(messages) // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-      const message2 = await message.channel.send("I have deleted `" + amount + "` messages!")
-      setTimeout(function () {
-        message2.delete()
-      }, timeout); 
-    });
+    
+    const amount = args[0]
+    if(isNaN(amount)) return message.channel.send('Please enter the amount of messages you want deleted')
+    
+    const fetched = await message.channel.fetchMessages( { limit : amount } )
+    
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.channel.send(`Error: ${error}))
+    
   }
 }
