8d4d5a8486e8edda54de3fb3d4723353794d6b91
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-06 21:14:05 -0400

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -4,30 +4,16 @@ module.exports = {
   run: async(Discord, client, message, args) => {
     if(!args[0]) return message.channel.send("Please mention a user to use this command on. @Example")
     let mention = args[0]
-    let user = getUserFromMention(mention, client)
+    let member = message.mentions.members.first()
     if(!user) return message.channel.send("Please enter the user as a mention. Ex: @Example")
     let guild = message.guild
 
     const embed = new Discord.MessageEmbed()
     .setAuthor(user.tag, user.displayAvatarURL())
     .setThumbnail(user.displayAvatarURL())
-    .addField("Account Created: ", user.createdAt, true)
-    .addField("Joined this Server: ", guild.members.get(user.id).joinedAt, true)
+    .addField("Account Created: ", member.user.createdAt, true)
+    .addField("Joined this Server: ", member.joinedAt, true)
     .setFooter(`Requested by ${message.author.tag} • Searched User`)
     .setTimestamp()
   }
-}
-
-function getUserFromMention(mention, client) {
-	if (!mention) return;
-
-	if (mention.startsWith('<@') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1);
-
-		if (mention.startsWith('!')) {
-			mention = mention.slice(1);
-		}
-
-		return client.users.cache.get(mention);
-	}
 }
