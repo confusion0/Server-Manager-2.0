758cfcd14e647ce7878753bcd682d98e7745fb0e
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-06 21:53:00 -0400

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -3,17 +3,30 @@ module.exports = {
   aliases: [],
   run: async(Discord, client, message, args) => {
     if(!args[0]) return message.channel.send("Please mention a user to use this command on. @Example")
-    let mention = args[0]
-    let member = message.mentions.members.first()
+    let rMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])) 
+    let user = message.mentions.users.first()
     if(!user) return message.channel.send("Please enter the user as a mention. Ex: @Example")
     let guild = message.guild
 
+    let createdAt = (user.createdAt).toString().slice(0, 14)
+    let joinedAt = (rMember.joinedAt).toString().slice(0, 14)
+    let roleAmount = rMember.roles.cache.size - 1
+    let id = user.id
+
+    let roles = ""
+    rMember.roles.cache.forEach(role => {
+      if(role.toString() != "@everyone") roles += role.toString() + " "
+    })
+
     const embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
     .setAuthor(user.tag, user.displayAvatarURL())
     .setThumbnail(user.displayAvatarURL())
-    .addField("Account Created: ", member.user.createdAt, true)
-    .addField("Joined this Server: ", member.joinedAt, true)
-    .setFooter(`Requested by ${message.author.tag} • Searched User`)
+    .addField(`Registered: `, createdAt, true)
+    .addField(`Joined: `, joinedAt, true)
+    .addField(`Roles[${roleAmount}]: `, roles)
+    .setFooter(`ID: ${id}`)
     .setTimestamp()
+    message.channel.send(embed)
   }
 }
