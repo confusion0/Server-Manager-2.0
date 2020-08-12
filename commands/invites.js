24235d66e78146968e1eae45f618c95c002ed133
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-12 13:58:22 -0400

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -2,26 +2,31 @@ module.exports = {
   name: 'invites',
   aliases: [],
   run: async (Discord, client, message, args) => {
-    const g = message.guild
-    let invites = await g.fetchInvites()
+    try{
+      const g = message.guild
+      let invites = await g.fetchInvites()
 
-    const embed = new Discord.MessageEmbed()
+      const embed = new Discord.MessageEmbed()
 
-    let userInvites = 0
+      let userInvites = 0
 
-    let iUser = message.mentions.users.first()
+      let iUser = message.mentions.users.first()
 
-    if(!iUser) iUser = message.author
+      if(!iUser) iUser = message.author
 
-    for(invite of invites){
-      invite = invite[1]
-      const inviter = client.users.cache.get(invite.inviter.id);
-      if(inviter === iUser) userInvites += invite.uses
+      for(invite of invites){
+        invite = invite[1]
+        const inviter = client.users.cache.get(invite.inviter.id)
+        if(inviter === iUser) userInvites += invite.uses
+      }
+
+      embed.setAuthor(iUser.tag, iUser.displayAvatarURL())
+      embed.setDescription(`${userInvites} Invites!`)
+
+      message.channel.send(embed)
     }
-
-    embed.setAuthor(iUser.tag, iUser.displayAvatarURL())
-    embed.setDescription(`${userInvites} Invites!`)
-
-    message.channel.send(embed)
+    catch(error e){
+      message.channel.send("Please contact the server owner and make sure that this bot has every permission in this server")
+    }
   }
 }
