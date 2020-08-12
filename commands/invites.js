2bdb7fa7e31a88374ffdd6309c2b4eb1ed14eb01
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-12 13:52:17 -0400

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -7,23 +7,21 @@ module.exports = {
 
     const embed = new Discord.MessageEmbed()
 
-    if(!args[0]) return message.channel.send(`Usage:   ${client.config.prefix}invites ${client.user}`)
-    else {
-      let userInvites = 0
-      let iUser = message.mentions.users.first()
+    let userInvites = 0
 
-      if(!iUser) return message.channel.send("please enter a valid mention")
+    let iUser = message.mentions.users.first()
 
-      for(invite of invites){
-        invite = invite[1]
-        const inviter = client.users.cache.get(invite.inviter.id);
-        if(inviter === iUser) userInvites += invite.uses
-      }
+    if(!iUser) iUser = message.author
 
-      embed.setAuthor(iUser.tag, iUser.displayAvatarURL())
-      embed.setDescription(`${userInvites} Invites!`)
-
-      message.channel.send(embed)
+    for(invite of invites){
+      invite = invite[1]
+      const inviter = client.users.cache.get(invite.inviter.id);
+      if(inviter === iUser) userInvites += invite.uses
     }
+
+    embed.setAuthor(iUser.tag, iUser.displayAvatarURL())
+    embed.setDescription(`${userInvites} Invites!`)
+
+    message.channel.send(embed)
   }
 }
