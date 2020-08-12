281821c7c6d97ea644761e7c4891c1fd3e92280c
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-12 16:27:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const fs = require('fs')
 
 client.config = require("./config.json")
 client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+client.invites = []
 
 const token = process.env.TOKEN;
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 client.commandFiles = commandFiles;
+client.eventFiles = eventFiles;
 
-console.log('\x1b[1m%s\x1b[0m', 'Embeds Loaded:');
+console.log('\x1b[1m%s\x1b[0m', 'Commands Loaded:');
 for (const file of commandFiles) {
-  const commands = require(`./commands/${file}`);
-  client.commands.set(commands.name, commands);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  const loaded = [`${file}`];
+  console.log(loaded);
+}
+
+console.log();
+
+console.log('\x1b[1m%s\x1b[0m', 'Events Loaded:');
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  client.events.set(event.name, event);
   const loaded = [`${file}`];
   console.log(loaded);
 }
@@ -22,40 +36,15 @@ for (const file of commandFiles) {
 console.log();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`)
-  client.user.setActivity('with !help');
+  return client.events.get("ready").run(Discord, client);
+})
+
+client.on('guildMemberAdd', member => {
+  return client.events.get("guildMemberAdd").run(Discord, client, member);
 })
 
 client.on('message', message => {
-  if (!message.guild) return;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(client.config.prefix)) return;
-
-//   var hasPerms = false
-//   client.config.whitelist.forEach((user) => {
-//     if(user.id === message.author.id){
-//       hasPerms = true
-//     }
-//   });
-//   const embed = new Discord.MessageEmbed()
-//   .setTitle("Access Denied")
-//   .setDescription("Please contact <@564177349990416484> in order to gain access to this bot.")
-//   if(!hasPerms) return message.channel.send(embed)
-
-  console.log(message.content + ' -- ' + message.author.tag + " -- " + (message.guild).toString());
-
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-
-  for (const file of client.commandFiles) {
-    const command = require(`./commands/${file}`);
-    var runCmd = false;
-    if (cmd === command.name) runCmd = true;
-    for(const alias of command.aliases){
-      if(cmd === alias) runCmd = true;
-    }
-    if(runCmd) return client.commands.get(command.name).run(Discord, client, message, args);
-  }
+  return client.events.get("message").run(Discord, client, message);
 })
 
 client.login(token);
