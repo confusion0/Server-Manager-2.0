dbc4e9a0e0227b695fe5b466320294f498b96025
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-12 16:26:32 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+
+module.exports = {
+  name: 'ready',
+  run: async(Discord, client) => {
+    console.log(`Logged in as ${client.user.tag}`)
+
+    let i = 0
+
+    setInterval(function(){
+      let activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`]
+
+      client.user.setActivity(activities[i], { type : "WATCHING" })
+
+      i = (i + 1) % activities.length
+    }, 5000);
+
+    wait(1000);
+
+    client.guilds.forEach(g => {
+      g.fetchInvites().then(guildInvites => {
+        client.invites[g.id] = guildInvites;
+      });
+    });
+  }
+}
