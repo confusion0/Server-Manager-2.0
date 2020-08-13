4937f127368c0bcf22bca9fb2a14bbc218cc7ae7
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-13 10:27:35 -0400

    Update inviteCreate.js

--- events/inviteCreate.js
+++ events/inviteCreate.js
@@ -4,7 +4,7 @@ module.exports = {
     const logChannel = invite.guild.channels.cache.find(channel => channel.name === "invite-logs");
     if(!logChannel) return
     const inviter = client.users.cache.get(invite.inviter.id);
-    if(invite.maxAge != 0) logChannel.send(`\`${invite.inviter.tag}\` please remake your invite as a invite that doesn't expire`)
-    if(invite.maxUses != 0) logChannel.send(`\`${invite.inviter.tag}\` please remake your invite as a invite that has infinite uses`)
+    if(invite.maxAge != 0) logChannel.send(`\`${invite.inviter}\` please remake your invite as a invite that doesn't expire`)
+    if(invite.maxUses != 0) logChannel.send(`\`${invite.inviter}\` please remake your invite as a invite that has infinite uses`)
   }
 }
