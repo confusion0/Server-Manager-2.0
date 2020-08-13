c1e432e8293d18c4c0735fd13cbdc75f3f440371
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-13 09:35:40 -0400

    Delete join.js

--- commands/join.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'join',
-  aliases: [],
-  run: async(Discord, client, message, args) => {
-    return client.events.get("guildMemberAdd").run(Discord, client, message.member);  
-  }
-}
