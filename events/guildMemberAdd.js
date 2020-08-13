97f2e2de529d071cc28121be7b9a315f1f946c62
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-13 10:30:31 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -21,7 +21,7 @@ module.exports = {
       const logChannel = member.guild.channels.cache.find(channel => channel.name === "invite-logs");
       if(!logChannel) return
       // A real basic message with the information we need. 
-      logChannel.send(`${member.user} joined using invite code \`${invite.code}\` from ${inviter}. **(${invite.uses} Invites)**`);
+      logChannel.send(`${member.user} **joined**; Invited by **${inviter.tag}** (${invite.uses} Invites)`);
     });
   }
 }
