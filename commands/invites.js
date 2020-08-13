2ece9d44307cf6f45ed271926cd5870c8b6d86e8
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-13 14:59:21 -0400

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -14,7 +14,7 @@ module.exports = {
 
         let iUser = message.mentions.users.first()
 
-        if(!iUser) iUser = message.author
+        if(!iUser) return message.channel.send('Please mention the user you would like to check')_
 
         for(invite of invites){
           invite = invite[1]
