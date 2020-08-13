cca2f33861cc7d2e15ea77f71f7cc78632658e4b
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2020-08-13 11:50:00 -0400

    Update counting.js

--- events/counting.js
+++ events/counting.js
@@ -1,6 +1,23 @@
+const timeout = 5000
 module.exports = {
   name: 'counting',
   run: async(Discord, client, message) => {
-
+    const logChannel = message.guild.channels.cache.find(channel => channel.name === "counting");
+    if(!logChannel) return
+    if(logChannel.id != message.channel.id) return
+    
+    const lastMessage = await message.channel.messages.fetch({limit : 1})
+    
+    if(isNaN(lastMessage.content)) return
+    
+    if(isNaN(message.content)){
+      message.delete()
+      const message2 = await message.channel.send("This is a counting channel please refrain from chatting anything else")
+      message2.delete({timeout : timeout})
+    }
+    
+    if(lastMessage.content + 1 == message.content) return
+    
+    message.delete()
   }
 }
