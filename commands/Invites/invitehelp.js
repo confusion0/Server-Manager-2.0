10f43346493aac58d837020e9751193eafae5210
Thomas Zhou <doomking9000@gmail.com>
2020-08-30 15:29:11 +0000

    FIXED INVITEHELP.JS

--- commands/Invites/invitehelp.js
+++ commands/Invites/invitehelp.js
@@ -5,14 +5,28 @@ module.exports = {
   args: "",
   desc: "Sends you a help message.",
   run: async (Discord, client, message, args) => {
-    message.reply("check your DM's")
-    message.author.send("PC Permanent Invite: ", { files: [ "./files/Step1.png",
-    "./files/Step2.png",
-    "./files/Step3.png",
-    "./files/Step4.png",
-    "./files/Step5.png",]})
-    message.author.send("Mobile Permanent Invite: ", { files: [
-      "https://cdn.discordapp.com/attachments/747924173879246888/749472167539769384/video0.mp4"
-    ]})
+    if(!args[0]) message.reply("Please include pc, android, or apple")
+    if(args[0] =="pc") {
+      message.reply("check your DM's")
+      message.author.send("PC Permanent Invite: ", { files: [ "./files/InvitePCStep1.png",
+      "./files/InvitePCStep2.png",
+      "./files/InvitePCStep3.png",
+      "./files/InvitePCStep4.png",
+      "./files/InvitePCStep5.png",]})
+    }
+    else if(args[0] == "android"){
+      message.reply("check your DM's")
+      message.author.send("Android Permanent Invite: ", { files: [ "./files/InviteAndroidStep1.jpg",
+      "./files/InviteAndroidStep2.jpg",
+      "./files/InviteAndroidStep3.jpg",
+      "./files/InviteAndroidStep4.jpg",]})
+    }
+    else if(args[0] == "apple"){
+      message.reply("check your DM's")
+      message.author.send("Apple Permanent Invite: ", { files: [ "https://cdn.discordapp.com/attachments/747924173879246888/749472167539769384/video0.mp4"]})
+    }
+    else{
+      message.reply("Please include pc, android, or apple")
+    }
   }
 }
