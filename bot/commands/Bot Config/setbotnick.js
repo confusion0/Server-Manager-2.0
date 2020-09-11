2e9ae7a37d04bb0d86fc10b97ffa79a789f860db
Thomas Zhou <doomking9000@gmail.com>
2020-09-11 17:30:06 +0000

    update 5.4

--- bot/commands/Bot Config/setbotnick.js
+++ bot/commands/Bot Config/setbotnick.js
@@ -6,7 +6,7 @@ module.exports = {
   desc: "Sets the nickname of the bot for this server.",
   run: async(Discord, client, message, args) => {
     if(!args[0]) message.channel.send("Please include the nickname you would like to change the current one to.")
-    message.guild.members.cache.get(client.user.id).setNickname(args[0])
-    message.channel.send(`Server nickname has been changed to \`${args[0]}\``)
+    message.guild.members.cache.get(client.user.id).setNickname(args.join(" "))
+    message.channel.send(`Server nickname has been changed to \`${args.join(" ")}\``)
   }
 }
