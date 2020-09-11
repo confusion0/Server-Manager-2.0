edfd04c6d886e5b195af5ac373c4981bb80c3f57
Thomas Zhou <doomking9000@gmail.com>
2020-09-11 15:25:54 +0000

    Update 5.2

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const path = require("path")
 const express = require("express")
 const chalk = require('chalk')
-const { mongostatus } = require('./mongo.js')
-
-displaymongostatus()
 
 const server = express()
 
@@ -11,9 +8,3 @@ const websiteRoutes = require('./website/routes')
 
 server.use('/', websiteRoutes)
 server.listen(process.env.PORT || 3000, function() { console.log(chalk.green("Web Server is Ready"))})
-
-async function displaymongostatus(){
-  const status = await mongostatus()
-  if(status) console.log(chalk.green('Connected to Mongo'))
-  else console.log(chalk.red('Mongo Connection Error'))
-}
