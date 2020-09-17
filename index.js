07e67463bf11245e22ff2e1b2cc2b721be03e0e4
Thomas Zhou <doomking9000@gmail.com>
2020-09-17 13:32:58 +0000

    added try catch to index to try and fix problem

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const chalk = require('chalk');
 const { ShardingManager } = require('discord.js');
+try {
+  const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN, totalShards: 'auto' });
 
-process.on('UnhandledPromiseRejectionWarning', function (err) {
-  console.log("Taco Beaer")
-})
+  manager.on('shardCreate', shard => {
+    shard.on("ready", () => {
+        console.log(`Shard ${shard.id} connected to Discord's Gateway.`)
+        shard.send({type: "shardId", data: {shardId: shard.id}});
+    });
+  })
 
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN, totalShards: 'auto' });
-
-manager.on('shardCreate', shard => {
-  shard.on("ready", () => {
-      console.log(`Shard ${shard.id} connected to Discord's Gateway.`)
-      shard.send({type: "shardId", data: {shardId: shard.id}});
-  });
-})
-
-manager.spawn();
+  manager.spawn();
+} catch (error){
+  console.log("CAUGHT A ERROR!")
+  console.log(error)
+}
 
 
