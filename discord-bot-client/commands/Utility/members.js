d9a671672a600a79ed651e15c74a2e622bffe29b
Thomas Zhou <doomking9000@gmail.com>
2020-10-15 12:30:38 +0000

    Better members.js command

--- discord-bot-client/commands/Utility/members.js
+++ discord-bot-client/commands/Utility/members.js
@@ -1,11 +1,22 @@
 const { CanvasRenderService } = require('chartjs-node-canvas')
-const { MessageAttachment } = require('discord.js')
+const { MessageAttachment, MessageEmbed, MessageEmbedImage } = require('discord.js')
 const ChartJS = require('chart.js')
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 const width = 600
 const height = 800
 
-const chartCallback = (ChartJS) => {}
+const chartCallback = (ChartJS) => {
+  ChartJS.plugins.register({
+    beforeDraw: (chartInstance) => {
+      const { chart } = chartInstance
+      const { ctx } = chart
+      ctx.fillStyle = 'white'
+      ctx.fillRect(0, 0, chart.width, chart.height)
+    }
+  })
+}
 
 module.exports = {
   name: 'members',
@@ -27,7 +38,18 @@ module.exports = {
     var members = []
     var dates = []
 
-    const data = Object.values(guildMembersData)
+    var data = Object.values(guildMembersData)
+    // if(args[0] == 'test'){
+    //   data = [
+    //     { date: '10/14/2020', members: 1002 },
+    //     { date: '10/15/2020', members: 2022 },
+    //     { date: '10/16/2020', members: 3455 },
+    //     { date: '10/17/2020', members: 4937 },
+    //     { date: '10/18/2020', members: 6535 },
+    //     { date: '10/19/2020', members: 9243 },
+    //     { date: '10/20/2020', members: 12312 }
+    //   ]
+    // }
 
     for(const item of data){
       members.push(item.members)
@@ -37,21 +59,40 @@ module.exports = {
 
     const configuration = {
       type: 'line',
+
+      borderCapStyle: 'round',
+      lineTension: 0.4,
+
       data: {
         labels: dates,
         datasets: [
           {
-            label: 'Discord Members',
-            data: members,
+            label: 'Members',
             backgroundColor: '#7289d9',
-          }
+            borderColor: '#7289d9',
+            showLine: true,
+            fill: false,
+            data: members,
+          },
         ],
+      },
+      options: {
+        title: {
+          display: true,
+          text: `${message.guild.toString()}`,
+        },
       },
     }
 
     const image = await canvas.renderToBuffer(configuration)
 
     const attachment = new MessageAttachment(image)
+
+    // const embed = new MessageEmbed()
+    // .setTitle(":family: Members")
+    // .setDescription("Total: " + message.guild.memberCount)
+
+    // message.channel.send(embed)
 
     message.channel.send(attachment)
   }
