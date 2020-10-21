57ec957e98a7035271e2237692866eeaffc9de27
Thomas Zhou <doomking9000@gmail.com>
2020-10-21 16:11:48 +0000

    Added cencors to eligible.js

--- discord-bot-client/commands/Secret/eligible.js
+++ discord-bot-client/commands/Secret/eligible.js
@@ -7,13 +7,13 @@ module.exports = {
   example: [],
   cooldown: 2000,
   run: async(client, message, args) => {
-    var eligible = false
     const ownerID = message.author.id
 
     const promises = [ client.shard.fetchClientValues('guilds.cache') ]
 
     return Promise.all(promises)
       .then(results => {
+        var messages = []
         var guilds = []
         for( guild of results[0][0]){
           if(guild.ownerID == ownerID) guilds.push(guild)
@@ -21,9 +21,10 @@ module.exports = {
         if(guilds.length < 1) {
           return message.channel.send("You are ineligible to participate this giveaways that require you to add me. Use the `help` command to invite me.")
         }
-        message.channel.send('Looks like you are eligible to enter giveaways that require you to add me! Congrats, below are your qualifiying servers.')
+        message.channel.send('Looks like you are eligible to enter giveaways that require you to add me! Congrats, below are your qualifiying servers. Messages be cencored in 5 secs due to privacy reasons.')
         for( guild of guilds ){
           message.channel.send(`\`${guild.name}\` is owned by \`${guild.ownerID}\`, called \`${guild.name}\``)
+            .then(msg => msg.delete({timeout: 5000}))
         }
       })
       .catch(console.error);
