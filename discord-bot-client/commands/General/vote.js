2d9bf29742225e3e9a33e318d41d68c908346ff0
Thomas Zhou <doomking9000@gmail.com>
2020-12-02 19:21:23 +0000

    Update 10.0.17.10

--- discord-bot-client/commands/General/vote.js
+++ discord-bot-client/commands/General/vote.js
@@ -16,10 +16,11 @@ module.exports = {
   run: async(client, message, args) => {
     const embed = new MessageEmbed()
       .setTitle(`Vote for ${client.user.username}`)
+      .setColor('RANDOM')
 
     const topgg_last_vote = await client.uData.get(`${message.author.id}:votes:top.gg`)
-    if(!topgg_last_vote || new Date().getTime() - topgg_last_vote > 12 * 60 * 60 * 1000){
-      embed.addField('top.gg', `[AVAILABLE NOW!](https://top.gg/bot/739943852726681650/vote)`)
+    if(!topgg_last_vote || new Date().getTime() - topgg_last_vote > twelve_hours){
+      embed.addField('top.gg', `\`[AVAILABLE NOW!](https://top.gg/bot/739943852726681650/vote)\``)
     }
     else {
       embed.addField('top.gg', `\`${ms(twelve_hours - (new Date().getTime() - topgg_last_vote))} remaining\``)
