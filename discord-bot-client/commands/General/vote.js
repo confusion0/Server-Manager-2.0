c1fb7fcbfc2ab0d826545190b518b001d6012226
Thomas Zhou <doomking9000@gmail.com>
2020-12-02 18:16:56 +0000

    Update 10.0.17.3

--- discord-bot-client/commands/General/vote.js
+++ discord-bot-client/commands/General/vote.js
@@ -15,8 +15,8 @@ module.exports = {
     const embed = new MessageEmbed()
       .setTitle(`Vote for ${client.user.username}`)
 
-    const topgg_last_vote = client.gData.get(`${message.author.id}:votes:top.gg`) || 0
-    if(new Date().getTime() - topgg_last_vote > 12 * 60 * 60 * 1000){
+    const topgg_last_vote = client.gData.get(`${message.author.id}:votes:top.gg`)
+    if(!topgg_last_vote || new Date().getTime() - topgg_last_vote > 12 * 60 * 60 * 1000){
       embed.setDescription('top.gg', `[AVAILABLE NOW!](https://top.gg/bot/739943852726681650/vote)`)
     }
     else {
