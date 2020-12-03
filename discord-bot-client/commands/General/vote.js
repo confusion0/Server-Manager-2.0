5427b934422ed7535550150c16d0ceb57fab9002
Thomas Zhou <doomking9000@gmail.com>
2020-12-03 20:03:31 +0000

    Update 10.0.17.13

--- discord-bot-client/commands/General/vote.js
+++ discord-bot-client/commands/General/vote.js
@@ -20,7 +20,7 @@ module.exports = {
 
     const topgg_last_vote = await client.uData.get(`${message.author.id}:votes:top.gg`)
     if(!topgg_last_vote || new Date().getTime() - topgg_last_vote > twelve_hours){
-      embed.addField('top.gg', `\`[AVAILABLE NOW!](https://top.gg/bot/739943852726681650/vote)\``)
+      embed.addField('top.gg', `[AVAILABLE NOW!](https://top.gg/bot/739943852726681650/vote)`)
     }
     else {
       embed.addField('top.gg', `\`${ms(twelve_hours - (new Date().getTime() - topgg_last_vote))} remaining\``)
