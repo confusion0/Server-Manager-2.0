0445ab29922b745f8b1fdf72751f36c1f878caae
confusion <74419921+confusion0@users.noreply.github.com>
2021-08-18 20:33:26 -0400

    Update help.js

--- discord-bot-client/commands/General/help.js
+++ discord-bot-client/commands/General/help.js
@@ -1,78 +1,104 @@
+const timeout = 5000
 const { MessageEmbed } = require('discord.js')
-const path = require('path')
 
-const addInfo = `
-[Invite Bot](https://discord.com/api/oauth2/authorize?client_id=739943852726681650&permissions=8&scope=bot)
-[Support Server](https://discord.gg/WftFPZc)
-[Website](https://servermanager20.herokuapp.com/)
-`
+colors = [
+	@@ -35,8 +35,8 @@ colors = [
+]
 
 module.exports = {
-  name: 'help',
-  aliases: [],
-  reqPerm: "NONE",
-  args: "[command]",
-  cooldown: 1000,
-  module: "General",
-  desc: "Shows all the commands and how to use them.",
-  example: ['', 'ping', 'snipe'],
-  run: async(client, message, args) => {
-    const embed = new MessageEmbed();
-    const serverprefix = (await client.gData.get(`${message.guild.id}:prefix`)) || process.env.PREFIX
-    if(!args[0]){
-      let modules = []
-      embed.setTitle("📚 Help")
-      embed.setDescription(`For more info in a command do ${serverprefix}help <command-name> \nPrefix: \`${serverprefix}\``)
-      
-      client.commands.forEach(command => {
-        const filepath = client.commandFiles.find(filepath => filepath.includes(command.name))
-        if(!filepath) return
-        
-        const module = getModuleFromPath(filepath)
-        
-        if(module == "Secret") return
-        
-        var field = embed.fields.find(field => field.name == module)
-        
-        if(!field) {
-          embed.addField(module, 'NONE')
-          field = embed.fields.find(field => field.name == module)
+  name: 'custom-embed',
+  aliases: ['custom-embeds', 'ce'],
+  reqPerm: "MANAGE_GUILD",
+  args: "",
+  cooldown: 3000,
+	@@ -56,7 +56,7 @@ module.exports = {
+
+    const embed = new MessageEmbed()
+    .setTitle("Custom Embed Creator")
+    .setColor('RANDOM')
+    .setFooter(`Step ${step} of ${steps} | Type cancel to exit • ${message.author.tag}`)
+    .setDescription(text[0])
+    .addFields(
+	@@ -66,8 +66,8 @@ module.exports = {
+      { name: 'Channel', value: channel},
+	  )
+
+    embed.setFooter(`Step ${step+1} of ${steps} | Type cancel to exit`)
+    const message2 = await message.channel.send(embed)
+
+    collector.on('collect', async m => {
+      if(m.author.bot) return
+	@@ -82,7 +82,7 @@ module.exports = {
+      if(step == 0){
+        title = (m.content).trim()
+        if(title.length > 1024){
+          let errorMessage = await message.channel.send(`The **title** cannot be longer than 1024 characters.`)
+          return messages.push(errorMessage)
         }
-        if(field.value == "NONE") field.value = '`'+ command.name +'`'
-        else field.value += `, \`${command.name}\``
-      })
-      
-      embed.addField("Additional Information", addInfo)
+        if(title === "empty") {
+	@@ -94,7 +94,7 @@ module.exports = {
+      if(step == 1){
+        description = (m.content).trim()
+        if(description.length > 1024){
+          let errorMessage = await message.channel.send("The **description** cannot be longer than 1024 characters.")
+          return messages.push(errorMessage)
+        }
+        if(description === "empty") {
+	@@ -106,15 +106,15 @@ module.exports = {
+      if(step == 2){
+        color = (m.content).trim()
+        if(!(color.startsWith("#") && color.length === 7) && !colors.includes(color.toUpperCase())){
+          let errorMessage = await message.channel.send("The **color** you inputed is not valid")
+          return messages.push(errorMessage)
+        }
+        embed.fields.find(c => c.name === 'Color')['value'] = color.toUpperCase();
+      }
+      if(step == 3){
+        channel = (m.content).trim()
+        if(!(channel === "current" || channel.startsWith("<#") && channel.endsWith(">"))){
+          let errorMessage = await message.channel.send("The **channel** you inputed is not valid")
+          return messages.push(errorMessage)
+        }
+        embed.fields.find(c => c.name === 'Channel')['value'] = channel;
+	@@ -132,35 +132,27 @@ module.exports = {
+        return collector.stop()
+      }
+    });
 
-      return message.channel.send(embed)
-    }
+    collector.on('end', async collected => {
+      message.delete({timeout : timeout})
+      setTimeout(function () {
+        messages.forEach((msg) => {
+          msg.delete().catch(() => {})
+        });
+      }, timeout); 
 
-    var command = client.commands.get(args[0])
+      if(cancelled){
+        const message3 = await message.channel.send(`Custom Embed Creation Cancelled. All messages with self destruct in ${timeout/1000} secs`)
+        return message3.delete({timeout: timeout}).then(message2.delete({timeout: timeout}))
+      } 
 
-    if(!command) return message.channel.send('I could not find a command with that name.')
+      const message4 = await message.channel.send(`Thank you for using my custom embed creator. All messages will now self destruct in ${timeout/1000} secs.`)
 
-    embed.setTitle(`Command Information - ${command.name}`)
-    embed.setDescription("<> means required, and [] means optional")
-    embed.addField("Description: ", command.desc)
-    embed.addField("Usage: ", serverprefix + command.name + " " + command.args)
-    console.log(command)
-    if(command.aliases.length > 0) embed.addField("Aliases: ", command.aliases.join(" "))
-    if(command.reqPerm != "NONE") embed.addField("Required Permissions: ", command.reqPerm)
-    if(command.example.length > 0) {
-      var examples = ""
-      command.example.forEach(example => {
-        examples += ( serverprefix + command.name + " " + example + ", ")
-      })
-      examples.substring(0, examples.length-2)
-      embed.addField('Examples', examples)
-    }
-    if(command.cooldown) embed.addField('Cooldown: ', command.cooldown)
-    message.channel.send(embed)
+      message4.delete({timeout: timeout}).then(message2.delete({timeout: timeout}))
+
+      const embed1 = new MessageEmbed()
+      .setTitle(title)
+      .setDescription(description)
+      .setColor(color.toUpperCase())
+
+      if(channel.toLowerCase() === "current") message.channel.send(embed1)
+      else getChannelFromMention(client, message, channel).send(embed1)
+    });
   }
 }
 
-const getModuleFromPath = (filepath) => {
-  const splited = filepath.split(path.sep)
-  return splited[splited.length-2]
+function getChannelFromMention(client, message, mention) {
+	if (!mention) return;
+
+	@@ -172,5 +164,5 @@ function getChannelFromMention(client, message, mention) {
+		}
+
+		return message.guild.channels.cache.get(mention);
+	}
 }
-  
